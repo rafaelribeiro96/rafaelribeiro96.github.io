1285e2fbfc92c3e2ccbce60802b57e5f7ef7de81
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-10-07 12:11:32 -0300

    corrigindo redirecionamento da página

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 // import Provider from './context/Provider';
-import Main from './page/Main';
+import MainPage from './page/MainPage';
 
 function App() {
   return (
     // <Provider>
     <Switch>
-      <Route exact path="/" component={ Main } />
+      <Route exact path="/" component={ MainPage } />
       {/* <Redirect to="/" /> */}
     </Switch>
     // </Provider>
