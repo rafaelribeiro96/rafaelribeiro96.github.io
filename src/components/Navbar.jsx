76c9d9bc68e923c515f7ec80562e5d98e8e9a93f
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-10-07 15:05:23 -0300

    Criada mudança de cor e tema utilizando CSS e HTML do React puro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 // import React, { useContext } from 'react';
 // import context from '../contexts/Context';
 import './Navbar.css';
+import { themeData } from '../data/themeData';
+import { ThemeContext } from '../context/ThemeContext';
 
 export default function Navbar() {
+  const {
+    themeColor, selectThemeColor, theme, selectThemeType,
+  } = useContext(ThemeContext);
+
   return (
     <div id="header">
       <div>Navbar</div>
+
+      <div className="button-theme-option">
+
+        <form>
+          <label
+            className="theme-color-checkbox-title"
+            htmlFor="checkbox-theme"
+          >
+            Dark Mode
+            <input
+              id="checkbox-theme"
+              type="checkbox"
+              checked={ theme.type === 'dark' }
+              onChange={ selectThemeType }
+            />
+          </label>
+          <label
+            className="theme-color-select-title"
+            htmlFor="theme-color-select"
+          >
+            Theme Color
+
+            <select
+              id="theme-color-select"
+              value={ themeColor }
+              onChange={ selectThemeColor }
+            >
+              {Object.keys(themeData).map((color) => (
+                <option key={ color } value={ color }>
+                  {color.charAt(0).toUpperCase() + color.slice(1)}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
+      </div>
+
     </div>
   );
 }
