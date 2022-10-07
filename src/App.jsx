edc59145b560e7f40a7754e0f22b7b8ac02a6251
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-10-07 09:55:43 -0300

    ambiente montado para iniciar criação do portfolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom';
+// import Provider from './context/Provider';
+import Main from './page/Main';
+
+function App() {
+  return (
+    // <Provider>
+    <Switch>
+      <Route exact path="/" component={ Main } />
+      <Redirect to="/" />
+    </Switch>
+    // </Provider>
+  );
+}
+
+export default App;
---
+++ src/context/Provider.jsx
@@ -0,0 +1,19 @@
+/* import React from 'react';
+// import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+function Provider({ children }) {
+  return (
+    <Context.Provider value={ value }>
+      {children}
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
+
+export default Provider;
+ */
