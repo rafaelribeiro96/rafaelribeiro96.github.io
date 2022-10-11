cd17d60970c059a5cf0e14233e6f489bf74d2574
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-10-10 21:37:49 -0300

    footer criado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-// import React, { useContext } from 'react';
-// import context from '../contexts/Context';
+import React, { useContext } from 'react';
 import './Footer.css';
+import { ThemeContext } from '../context/ThemeContext';
+import { headerData } from '../data/headerData';
 
 export default function Footer() {
+  const shortname = (name) => {
+    // eslint-disable-next-line no-magic-numbers
+    if (name.length > 10) {
+      return name.split(' ')[0];
+    }
+    return name;
+  };
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div id="footer">
-      <div>Footer</div>
+    <div className="footer" style={ { backgroundColor: theme.secondary } }>
+      <p style={ { color: theme.tertiary } }>
+        Made by
+        {' '}
+        {shortname(headerData.name)}
+      </p>
     </div>
   );
 }
