fcf3b57675eb664a8908a408912978056b27326e
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-01 14:54:22 -0300

    atualização de about

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,8 +1,9 @@
 /* eslint-disable max-len */
 export const aboutData = {
   title: 'Quem sou eu',
-  description1: 'Meu nome é Rafael Ribeiro, sou Técnico em Informática, Advogado Pós Graduado em Direito Digital e futuro Desenvolvedor Web Full-Stack, formado pela Trybe',
+  description1: 'Meu nome é Rafael Ribeiro, sou Desenvolvedor Web Front-End, Técnico em Informática e Advogado Pós Graduado em Direito Digital. Estou finalizando a formação Full Stack na Trybe e iniciei em 2023 a Trilha Web Full Stack da Alura através do Programa Desenvolve do Grupo Boticário.',
   description2: 'A Trybe é uma escola de tecnologia, que proporciona uma formação com mais de 1500 horas, abordando fundamentos de desenvolvimento Web, Front-end, Back-end, ciência da computação, metodologias ágeis e habilidades comportamentais.',
-  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que na Trybe estudamos Ferramentas e Tecnologias como:  JavaScript, HTML 5, Css, Git, React, Redux, Jest, RTL, Docker, MySql, NodeJs, TypeScript, MongoDb e Python.',
+  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que na Trybe estudo as seguintes Ferramentas e Tecnologias: JavaScript, HTML 5, Css, Git, React, Redux, Figma, Jest, Docker, MySql, NodeJs, TypeScript, MongoDb e Python.',
   image: 1,
 }; export default aboutData;
+
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import resume from '../assets/pdf/resume.pdf';
 export const headerData = {
   name: 'Rafael Ribeiro',
   title: 'Desenvolvedor Web',
-  desciption: 'Comecei minha carreira na TI ainda na adolescência, quando concluí o curso técnico de informática junto do ensino médio. Neste primeiro momento atuei por um ano e meio com suporte e implantação de sistemas de automação comercial. Em seguida trabalhei na área administrativa e posteriormente atuei como advogado. Mesmo trabalhando em outras áreas, a tecnologia sempre esteve presente no meu dia a dia, inclusive na Pós Graduação que cursei em Direito Digital.  Depois disso resolvi mergulhar de vez no mundo da tecnologia, e estou me tornando um Desenvolvedor Web Full-Stack através do curso da Trybe.',
+  desciption: 'Sou um Desenvolvedor Web Front-End. Comecei minha carreira na TI ainda na adolescência, quando concluí o curso técnico de informática integrado ao ensino médio. Possuo experiência na área administrativa, jurídica e no processo de implantação de sistemas. Atualmente resolvi mergulhar de vez no mundo da tecnologia, e estou me tornando um Desenvolvedor Web Full-Stack através dos cursos da Trybe e da Alura, no qual tenho como foco principal o desenvolvimento Front-End.',
   image: 'https://i.postimg.cc/C5Lyzhb4/foto-rafael.jpg',
   resumePdf: resume,
 }; export default headerData;
