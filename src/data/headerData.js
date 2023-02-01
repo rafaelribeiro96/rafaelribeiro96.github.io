87cd5ed3a62815200a416255de9eb214a90fb379
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-02-01 14:33:47 -0300

    alterada imagem de perfil do home

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
   name: 'Rafael Ribeiro',
   title: 'Desenvolvedor Web',
   desciption: 'Comecei minha carreira na TI ainda na adolescência, quando concluí o curso técnico de informática junto do ensino médio. Neste primeiro momento atuei por um ano e meio com suporte e implantação de sistemas de automação comercial. Em seguida trabalhei na área administrativa e posteriormente atuei como advogado. Mesmo trabalhando em outras áreas, a tecnologia sempre esteve presente no meu dia a dia, inclusive na Pós Graduação que cursei em Direito Digital.  Depois disso resolvi mergulhar de vez no mundo da tecnologia, e estou me tornando um Desenvolvedor Web Full-Stack através do curso da Trybe.',
-  image: 'https://media-exp1.licdn.com/dms/image/D4D03AQHSmoimQByNfg/profile-displayphoto-shrink_800_800/0/1665018503780?e=1670457600&v=beta&t=rFSYWrV89a2HTwhVP32Ro5zkI-UTx1zUqwUHWu0hioY',
+  image: 'https://i.postimg.cc/C5Lyzhb4/foto-rafael.jpg',
   resumePdf: resume,
 }; export default headerData;
