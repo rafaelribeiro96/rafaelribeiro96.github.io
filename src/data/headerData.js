972fdf03d52386cb38a4441e4ed05c61beceedd2
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-03-20 12:43:21 -0300

    atualizado status

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,8 +3,8 @@ import resume from '../assets/pdf/resume.pdf';
 
 export const headerData = {
   name: 'Rafael Ribeiro',
-  title: 'Desenvolvedor Web',
-  desciption: 'Sou um Desenvolvedor Web Front-End. Comecei minha carreira na TI ainda na adolescência, quando concluí o curso técnico de informática integrado ao ensino médio. Possuo experiência na área administrativa, jurídica e no processo de implantação de sistemas. Atualmente resolvi mergulhar de vez no mundo da tecnologia, e estou me tornando um Desenvolvedor Web Full-Stack através dos cursos da Trybe e da Alura, no qual tenho como foco principal o desenvolvimento Front-End.',
+  title: 'Front End Engineer ',
+  desciption: 'Sou um Desenvolvedor Web Front-End. Comecei minha carreira na TI ainda na adolescência, quando concluí o curso técnico de informática integrado ao ensino médio. Possuo experiência na área administrativa, jurídica e no processo de implantação de sistemas. Em 2022 resolvi mergulhar de vez no mundo da tecnologia, e me tornei um Desenvolvedor Web Full-Stack através dos cursos da Trybe e da Alura, no qual tenho como foco principal o desenvolvimento Front-End. Atualmente estou trabalhando como Desenvolvedor JavaScript na Base2 Tecnologia.',
   image: 'https://i.postimg.cc/C5Lyzhb4/foto-rafael.jpg',
   resumePdf: resume,
 }; export default headerData;
