c5a2eb255de05bbcb5787fa1125ca95981d62063
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2023-07-19 00:59:41 -0300

    atualizado data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
   title: 'Quem sou eu',
   description1: 'Meu nome é Rafael Ribeiro, sou Desenvolvedor Web Front-End, Técnico em Informática e Advogado Pós Graduado em Direito Digital. Realizei a formação de Desenvolvimento Web Full Stack na Trybe e a Trilha Web Full Stack da Alura através do Programa Desenvolve do Grupo Boticário. Atualmente estou trabalhando como Desenvolvedor Front-End JavaScript na Base2 Tecnologia e realizando Pós Graduação em Engenharia de Software',
   description2: 'Através dos cursos que realizei, incluindo bootcamps, udemy, alura e agora na pós graduação aprendi diversos conteúdos como fundamentos de desenvolvimento Web, Front-end, Back-end, ciência da computação, metodologias ágeis e habilidades comportamentais.',
-  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que estudo algumas das seguintes Ferramentas e Tecnologias: JavaScript, React, NextJs, NodeJs, HTML 5, Css, Git, Redux, Figma, Jest, Docker, MySql, TypeScript, MongoDb e Python.',
+  description3: 'O mundo da Tecnologia está em constante transformação, e é por isso que estudo algumas das seguintes Ferramentas e Tecnologias: JavaScript, React, NextJs, NodeJs, HTML 5, Css, Git, Redux, Figma, Jest, Docker, MySql, TypeScript, MongoDb e Python. Porfólio profissional: www.rafaeldev.com',
   image: 1,
 }; export default aboutData;
