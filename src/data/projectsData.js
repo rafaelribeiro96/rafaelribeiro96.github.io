78339cd652a5c03bbe6f5f2599f7ca7cd6a87467
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2024-09-19 16:04:01 -0300

    Adição de projeto RoteiroPfeito

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,6 +11,7 @@ import seven from '../assets/png/projects/tryunfo.png';
 import eight from '../assets/png/projects/solarsystem.png';
 import ten from '../assets/png/projects/rafaeldev.png';
 import eleven from '../assets/png/projects/glayderibeiro.png';
+import twelve from '../assets/png/projects/roteiropfeito.png';
 
 export const projectsData = [
   {
@@ -28,53 +29,62 @@ export const projectsData = [
     projectDesc: 'Site comercial para divulgação de serviços de desenvolvimento web.',
     tags: ['NextJs', 'React', 'CSS', 'NodeJs', 'MongoDb'],
     code: 'https://github.com/rafaelribeiro96/',
-    demo: 'https://www.rafaeldev.com/',
+    demo: 'https://roteiropfeito.vercel.app/',
     image: ten,
   },
   {
     id: 3,
+    projectName: 'Roteiro Pfeito',
+    projectDesc: 'Elaboração de roteiros de viagens através de IA.',
+    tags: ['NextJs', 'React', 'CSS'],
+    code: 'https://github.com/rafaelribeiro96/roteiro-de-viagem',
+    demo: 'https://www.roteiropfeito.com/',
+    image: twelve,
+  },
+  {
+    id: 4,
     projectName: 'Pokedex',
     projectDesc: 'Pokedex é um projeto desenvolvido utilizando React,'
-    + 'Hooks e a API PokéApi e estilizado com CSS puro.'
-    + 'Com intuito de aprofundar meus conhecimentos em React,'
-    + 'Hooks e consumo de APIs externas.',
+      + 'Hooks e a API PokéApi e estilizado com CSS puro.'
+      + 'Com intuito de aprofundar meus conhecimentos em React,'
+      + 'Hooks e consumo de APIs externas.',
     tags: ['React', 'CSS', 'Context API', 'React Hooks'],
     code: 'https://github.com/rafaelribeiro96/pokedex-test',
     demo: 'https://pokedexrr.netlify.app/',
     image: nine,
   },
   {
-    id: 4,
+    id: 5,
     projectName: 'App de Receitas',
     projectDesc: 'App para dispositivos móveis em que você pode buscar por receitas'
-    + ' de pratos e drinks, salvar suas favoritas e muitas outras funcionalidades.',
+      + ' de pratos e drinks, salvar suas favoritas e muitas outras funcionalidades.',
     tags: ['React', 'CSS', 'Context API'],
     code: 'https://github.com/rafaelribeiro96/recipes-app',
     demo: 'https://rafaelribeiro96.github.io/recipes-app/#/',
     image: one,
   },
   {
-    id: 5,
+    id: 6,
     projectName: 'Trybe Tunes',
     projectDesc: 'Aplicação capaz de reproduzir músicas, criar lista de favoritas,'
-    + 'editar perfil de usuário, consumindo dados de uma API.',
+      + 'editar perfil de usuário, consumindo dados de uma API.',
     tags: ['React', 'CSS'],
     code: 'https://github.com/rafaelribeiro96/project-trybetunes',
     demo: 'https://rafaelribeiro96.github.io/project-trybetunes/',
     image: two,
   },
   {
-    id: 6,
+    id: 7,
     projectName: 'Trivia',
     projectDesc: 'Foi desenvolvido um jogo de perguntas e respostas baseado no jogo'
-    + 'Trivia utilizando React e Redux.',
+      + 'Trivia utilizando React e Redux.',
     tags: ['React', 'CSS', 'Redux'],
     code: 'https://github.com/rafaelribeiro96/project-trivia-react-redux',
     demo: 'https://rafaelribeiro96.github.io/project-trivia-react-redux/',
     image: three,
   },
   {
-    id: 7,
+    id: 8,
     projectName: 'Wallet',
     projectDesc: 'Carteira de controle de gastos com conversor de moedas.',
     tags: ['React', 'Redux'],
@@ -83,17 +93,17 @@ export const projectsData = [
     image: four,
   },
   {
-    id: 8,
+    id: 9,
     projectName: 'Star Wars Planets Search',
     projectDesc: 'Aplicação com uma lista com filtros de planetas do universo de'
-    + 'Star Wars usando Context API e Hooks para controlar os estados globais.',
+      + 'Star Wars usando Context API e Hooks para controlar os estados globais.',
     tags: ['React', 'Context API', 'React Hooks'],
     code: 'https://github.com/rafaelribeiro96/project-starwars-planets-search',
     demo: 'https://rafaelribeiro96.github.io/project-starwars-planets-search/',
     image: five,
   },
   {
-    id: 9,
+    id: 10,
     projectName: 'Shopping Cart',
     projectDesc: 'Carrinho de compras consumindo API do Mercado Livre',
     tags: ['JavaScript', 'CSS'],
@@ -102,7 +112,7 @@ export const projectsData = [
     image: six,
   },
   {
-    id: 10,
+    id: 11,
     projectName: 'Trybe Trunfo',
     projectDesc: 'Criador de cartas para jogo estilo Super Trunfo',
     tags: ['React', 'CSS'],
@@ -111,7 +121,7 @@ export const projectsData = [
     image: seven,
   },
   {
-    id: 11,
+    id: 12,
     projectName: 'Solar System',
     projectDesc: 'Aplicação para testar a utilização de JSX no React',
     tags: ['React'],
